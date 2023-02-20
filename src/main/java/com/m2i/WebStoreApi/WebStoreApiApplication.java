547644022574a2cc7e84f6ca867a99e5818d0d7d
Yoann ZAMORA <zamora.yoann@gmail.com>
2023-02-20 09:39:59 +0100

    Init

---
+++ src/main/java/com/m2i/WebStoreApi/WebStoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.m2i.WebStoreApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebStoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebStoreApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/m2i/WebStoreApi/WebStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m2i.WebStoreApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebStoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
