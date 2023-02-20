0820733dc0970760cf129cae255dec9e1c0caf08
Marina Garin <marina.garin21@gmail.com>
2023-02-20 11:41:59 +0100

    Entités Command

---
+++ src/main/java/com/m2i/WebStoreApi/entity/Payment.java
@@ -0,0 +1,5 @@
+package com.m2i.WebStoreApi.entity;
+
+public class Payment {
+
+}
---
+++ src/main/java/com/m2i/WebStoreApi/repository/CommandLineRepository.java
@@ -0,0 +1,9 @@
+package com.m2i.WebStoreApi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.m2i.WebStoreApi.entity.CommandLine;
+
+public interface CommandLineRepository extends JpaRepository<CommandLine, Integer>{
+
+}
